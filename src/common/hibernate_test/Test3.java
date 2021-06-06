724a2f965fff82352682560b971c4d17f80e86b3
Andrey <gavrplay@gmail.com>
2021-06-06 22:00:03 +0300

    hibernate

--- src/common/hibernate_test/Test3.java
+++ src/common/hibernate_test/Test3.java
@@ -5,7 +5,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test1 {
+import java.util.List;
+
+public class Test3 {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -14,13 +16,22 @@ public class Test1 {
 
         try {
             Session session = factory.getCurrentSession();
-            Employee emp = new Employee("Alexandr", "Ivanov", "IT", 600);
             session.beginTransaction();
-            session.save(emp);
+
+//            List<Employee> emps = session.createQuery("from Employee")
+//                    .getResultList();
+
+
+            List<Employee> emps = session.createQuery("from Employee " +
+                    "where name='Alexandr' AND salary > 650")
+                    .getResultList();
+
+            for (Employee epm : emps)
+                System.out.println(epm);
+
             session.getTransaction().commit();
 
             System.out.println("Done!");
-            System.out.println(emp);
         }
         finally {
             factory.close();
